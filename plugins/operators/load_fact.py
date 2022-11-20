85d2b510ce42c30380f023a8e873ff2352cbc98f
stovepilot <97940293+stovepilot@users.noreply.github.com>
2022-11-20 14:30:35 +0000

    Add files via upload

---
+++ plugins/operators/load_fact.py
@@ -0,0 +1,50 @@
+
+
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class LoadFactOperator(BaseOperator):
+
+    ui_color = '#F98866'
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id="",
+                 table_name="",
+                 sql_select="",
+                 append="",
+                 *args, **kwargs):
+
+        super(LoadFactOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id= redshift_conn_id
+        self.table_name= table_name
+        self.sql_select= sql_select
+        self.append = append
+
+    def execute(self, context):
+        
+        self.log.info('Load Operator: connecting to Redshift')
+        
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        sql_insert = f"""
+            INSERT INTO {self.table_name}
+            {self.sql_select}
+        """        
+        
+        if self.append == True:
+            
+            self.log.info(f'Appending data to fact table {self.table_name}')
+
+            redshift.run(sql_insert)
+
+        else:
+
+            self.log.info(f'Truncating and re-inserting data into fact table {self.table_name}.')
+            
+            sql_delete = "DELETE FROM {}".format(self.table_name)
+
+            redshift.run(sql_delete)
+
+            redshift.run(sql_insert)
